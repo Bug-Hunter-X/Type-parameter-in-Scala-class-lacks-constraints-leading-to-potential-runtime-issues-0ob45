74fe00ed34a204c0b2124763f204924a8ed459cd
BugHunter-X <askzenai@gmail.com>
2025-01-30 20:48:04 +0100

    Add MyClass.scala file

---
+++ MyClass.scala
@@ -0,0 +1,16 @@
+```scala
+class MyClass[T](val value: T) {
+  def printValue(): Unit = {
+    println(value)
+  }
+}
+
+object MainObject {
+  def main(args: Array[String]): Unit = {
+    val myInt = new MyClass(10)
+    val myString = new MyClass("Hello")
+    myInt.printValue()
+    myString.printValue()
+  }
+}
+```
